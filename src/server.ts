86fd62d6af184f52425c884533092b28349060d9
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-11-15 16:51:18 +0300

    add home page

--- src/server.ts
+++ src/server.ts
@@ -8,10 +8,8 @@ const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use('/', express.static(__dirname + '/../public'));
 app.use('/api', apiRouter);
-app.use('/', (req: Request, res: Response) => {
-    res.send('Hello');
-})
 
 app.listen(PORT, () => 
     console.log(`Server started on port ${PORT}...`));
