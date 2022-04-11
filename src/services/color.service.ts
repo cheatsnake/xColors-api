f3a0206fc897f21d491fddf4b8734060fbb84a6b
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-04-11 19:42:31 +0300

    Fix missing percentage sign

--- src/services/color.service.ts
+++ src/services/color.service.ts
@@ -1,31 +1,41 @@
 import { IColors } from "../types/color.types";
-import { generateHexColor, hueData, randomLightness, randomNumber } from "./helper.service";
+import {
+    generateHexColor,
+    hueData,
+    randomLightness,
+    randomNumber,
+} from "./helper.service";
 import ConverterService from "./converter.service";
 
 class ColorService {
-    getRandomColor(number = 1): IColors | IColors[]  {
+    getRandomColor(number = 1): IColors | IColors[] {
         const arr = [];
-        for(let i = 0; i < number; i++) {
+        for (let i = 0; i < number; i++) {
             const hex = generateHexColor();
             const rgb = ConverterService.convertHexToRgb(hex);
             const hsl = ConverterService.convertRgbToHsl(rgb);
-            arr.push({hex, rgb, hsl})
+            arr.push({ hex, rgb, hsl });
         }
         return arr.length > 1 ? arr : arr[0];
     }
-    getRandomShade(hue:string, type: string, number: number): IColors | IColors[] {
+    getRandomShade(
+        hue: string,
+        type: string,
+        number: number
+    ): IColors | IColors[] {
         const arr = [];
-    
+
         for (let i = 0; i < number; i++) {
             const saturation = randomNumber(100, 50);
-            const hsl = `hsl(${hueData(hue)}, ${saturation}%, ${randomLightness(type)})`;
+            const hsl = `hsl(${hueData(hue)}, ${saturation}%, ${randomLightness(
+                type
+            )}%)`;
             const rgb = ConverterService.convertHslToRgb(hsl);
             const hex = ConverterService.convertRgbToHex(rgb);
-            arr.push({hex, rgb, hsl})
+            arr.push({ hex, rgb, hsl });
         }
         return arr.length > 1 ? arr : arr[0];
     }
 }
-
 
 export default new ColorService();
